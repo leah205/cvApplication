c4094761775c5afda47e25aae3c4b45b2cb810aa
leah <leahtiktin@gmail.com>
2024-10-16 22:20:50 -0600

    do functionality for opening a section

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,36 @@ import General from "./components/General";
 import InfoContainer from "./components/infoContainer";
 
 function App() {
+  const [openSection, setOpenSection] = useState(0);
+  function editSection(index) {
+    console.log("hello");
+    setOpenSection(index);
+  }
   return (
     <>
       <div className="edit-info">
-        <InfoContainer title="General Information">
+        <InfoContainer
+          title="General Information"
+          editSection={editSection}
+          index={1}
+          isActive={openSection == 1}
+        >
           <General />
         </InfoContainer>
 
-        <InfoContainer title="Educational Experience"></InfoContainer>
+        <InfoContainer
+          title="Educational Experience"
+          editSection={editSection}
+          index={2}
+          isActive={openSection == 2}
+        ></InfoContainer>
 
-        <InfoContainer title="Previous Experience"></InfoContainer>
+        <InfoContainer
+          title="Previous Experience"
+          editSection={editSection}
+          index={3}
+          isActive={openSection == 3}
+        ></InfoContainer>
       </div>
     </>
   );
--- src/components/infoContainer.jsx
+++ src/components/infoContainer.jsx
@@ -1,17 +1,19 @@
 import "../styles/infoContainer.css";
 import dropDown from "../images/drop-down.svg";
-
-function InfoContainer({ title, children }) {
-  console.log(children);
+//perhaps changing onclick function by moving it to app?
+//also add to editSection when it is already active
+function InfoContainer({ title, children, editSection, isActive }) {
+  if (!isActive) children = null;
   return (
     <div className="info-container">
       <div className="header">
         <p>{title}</p>
         <button>
-          <img src={dropDown} alt="edit" />
+          <img src={dropDown} alt="edit" onClick={() => editSection(1)} />
         </button>
       </div>
       <div className="form-container">{children}</div>
+      {isActive && <button className="update-button">Update</button>}
     </div>
   );
 }
